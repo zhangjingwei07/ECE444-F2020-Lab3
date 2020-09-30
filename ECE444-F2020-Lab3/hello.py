5da50f7bf2d335198e76c9c52b55bb0ff8c44c9b
zhangjingwei07 <jwdaniel.zhang@mail.utoronto.ca>
2020-10-01 00:52:49 +0800

    Activity2: All parts completed

--- ECE444-F2020-Lab3/hello.py
+++ ECE444-F2020-Lab3/hello.py
@@ -11,6 +11,7 @@ from wtforms.fields.html5 import EmailField
 
 class NameForm(FlaskForm):
   name = StringField('What is your name?', validators=[DataRequired()])
+  email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
   submit = SubmitField('Submit')
 
 app = Flask(__name__)
@@ -26,12 +27,22 @@ def index():
   form = NameForm()
   if form.validate_on_submit():
     old_name = session.get('name')
+    old_email = session.get('email')
+    if 'utoronto' not in form.email.data:
+      flash('Please use your UofT email!')
+      session['email'] = None
+    elif (old_email is not None and old_email != form.email.data):
+      flash('Looks like you have changed your email!')
+      session['email'] = form.email.data
+    else:
+      session['email'] = form.email.data
+
     if old_name is not None and old_name != form.name.data:
       flash('Looks like you have changed your name!')
     session['name'] = form.name.data
     return redirect(url_for('index'))
   return render_template('index.html',
-                         current_time=datetime.utcnow(), form = form, name=session.get('name'))
+                         current_time=datetime.utcnow(), form = form, name=session.get('name'), email=session.get('email', None))
 @app.route('/user/<name>')
 def user(name):
   return render_template('user.html', name=name)
